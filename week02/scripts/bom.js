d171c3e5279bc741cb680ec86d6b1ad70839c603
Manuel Chave <manuelmariomanuel199@gmail.com>
2025-09-14 16:32:50 +0200

    Fished BOM top 10 aplication code

--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -1,10 +1,31 @@
 let imputFavChap = document.getElementById("favchap");
 let btnAddChap = document.getElementById("btnAddChap");
 let list = document.getElementById("list");
-let chapterEntry = document.createElement("li");
-const deletedButton  = document.createElement("button");
 
-chapterEntry.textContent = imputFavChap.value;
-deletedButton.textContent = "❌";
-chapterEntry.append(deletedButton);
-list.append(chapterEntry);
+
+btnAddChap.addEventListener("click",()=>{
+    if(imputFavChap.value.trim() === "") {
+       return imputFavChap.focus();
+    }
+
+    let chapterEntry = document.createElement("li");
+    const deletedButton  = document.createElement("button");
+
+    chapterEntry.textContent = imputFavChap.value;
+    deletedButton.textContent = "❌";
+    deletedButton.setAttribute("aria-label", "Remove Alma 5");
+
+    chapterEntry.append(deletedButton);
+    list.append(chapterEntry);
+    
+
+    deletedButton.addEventListener("click", ()=>{
+        list.removeChild(chapterEntry);
+        imputFavChap.focus();
+
+    })
+     imputFavChap.value = " ";
+     imputFavChap.focus();
+    
+});
+
