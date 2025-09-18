497c74ea4ee59814ff692c29c306fd2684a78f9b
Manuel Chave <manuelmariomanuel199@gmail.com>
2025-09-18 22:36:32 +0200

    Fixed nav responsivity and updated js code

--- scripts/temples.js
+++ scripts/temples.js
@@ -3,14 +3,22 @@ let menu = document.querySelector(".menu");
 let title = document.querySelector(".title");
 
 
+function updateLayout() {
+  if (window.innerWidth <= 768) {
 
-menu.addEventListener("click", ()=>{
-  if(window.matchMedia("(max-width:760px)").matches)
-    {
-        menu.classList.toggle("open");
-    const isOpen = menu.classList.contains("open")
+    nav.style.display = menu.classList.contains("open") ? "block" : "none";
+    title.style.display = menu.classList.contains("open") ? "none" : "block";
+  } else {
+ 
+    nav.style.display = "flex";
+    title.style.display = "block";
+  }
+}
 
-    title.style.display = isOpen ? "none" : " block";
-    nav.style.display = isOpen ? "block" :"none";
-    }
+window.addEventListener("resize", updateLayout);
+updateLayout();
+
+menu.addEventListener("click", () => {
+  menu.classList.toggle("open");
+  updateLayout(); 
 })
